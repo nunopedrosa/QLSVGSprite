aca7d42e1503d17a4d45adebc03643247ff421ac
Nuno Pedrosa <nuno.f.pedrosa@gmail.com>
2018-11-21 23:37:38 +0000

    First working version
    Supports SpriteSVG and SVG preview.

---
+++ QLSVGSprite/GeneratePreviewForURL.m
@@ -0,0 +1,101 @@
+#include <CoreFoundation/CoreFoundation.h>
+#include <CoreServices/CoreServices.h>
+#include <QuickLook/QuickLook.h>
+#import <Cocoa/Cocoa.h>
+
+OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options);
+void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
+
+/* -----------------------------------------------------------------------------
+   Generate a preview for file
+
+   This function's job is to create preview for designated file
+   ----------------------------------------------------------------------------- */
+
+OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
+{
+    @autoreleasepool {
+        NSError *theErr = nil;
+        NSURL *myURL = (__bridge NSURL *)url;
+        
+        // Load document data using NSStrings house methods
+        NSStringEncoding stringEncoding;
+        NSString *fileString = [NSString stringWithContentsOfURL:myURL usedEncoding:&stringEncoding error:&theErr];
+        
+        // We could not open the file, probably unknown encoding; try ISO-8859-1
+        if (!fileString) {
+            stringEncoding = NSISOLatin1StringEncoding;
+            fileString = [NSString stringWithContentsOfURL:myURL encoding:stringEncoding error:&theErr];
+            // Still no success, give up
+            if (!fileString) {
+                if (nil != theErr) {
+                    NSLog(@"Error opening the file: %@", theErr);
+                }
+                return noErr;
+            }
+        }
+        
+        // Parse the data if still interested in the preview
+        if (false == QLPreviewRequestIsCancelled(preview)) {
+            
+            // Create HTML of the data if still interested in the preview
+            if (false == QLPreviewRequestIsCancelled(preview)) {
+                NSMutableString *html = [[NSMutableString alloc] initWithString:@""];
+                if ([fileString containsString:@"<symbol"]) {
+                
+                // compose the html
+                [html appendString:@"<!DOCTYPE html>\n"];
+                [html appendString:@"<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\"><head>\n"];
+                [html appendString:@"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /></head>\n"];
+                NSError *error = NULL;
+                NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"(<symbol.*?symbol>)"
+                                                                                       options:(NSRegularExpressionCaseInsensitive|NSRegularExpressionDotMatchesLineSeparators)
+                                                                                         error:&error];
+                NSRegularExpression *regexSym = [NSRegularExpression regularExpressionWithPattern:@"<symbol\\s+id=[\"'](.*?)[\"']\\s+viewBox=[\"'](.*?)[\"'].*?>(.*?)<.symbol"
+                                                                                          options:(NSRegularExpressionCaseInsensitive|NSRegularExpressionDotMatchesLineSeparators)
+                                                                                            error:&error];
+
+                NSArray *matches = [regex matchesInString:fileString
+                                                  options:0
+                                                    range:NSMakeRange(0, [fileString length])];
+                for (NSTextCheckingResult *match in matches) {
+                    NSRange matchRange = [match range];
+                    NSString *symbol= [fileString substringWithRange:matchRange];
+                    NSArray *matchesSym = [regexSym matchesInString:symbol
+                                                            options:0
+                                                              range:NSMakeRange(0, [symbol length])];
+                    for (NSTextCheckingResult *matchSym in matchesSym) {
+                        NSString *id= [symbol substringWithRange:[matchSym rangeAtIndex:1]];
+                        NSString *vBox= [symbol substringWithRange:[matchSym rangeAtIndex:2]];
+                        NSString *paths= [symbol substringWithRange:[matchSym rangeAtIndex:3]];
+                        [html appendString:@"<div style='border:1px solid #AAAAAA; float:left; width:192px; height:100px; text-align:center; justify-content: center;'>"];
+                        [html appendString:id];
+                        [html appendString:@"<div style='width:48px; margin: 0 auto'>"];
+                        [html appendString:@"<svg class='nav__icon color-grey-dark-3' viewBox='"];
+                        [html appendString:vBox];
+                        [html appendString:@"'>"];
+                        [html appendString:paths];
+                        [html appendString:@"</svg></div></div>"];
+                    }
+                }
+                [html appendString:@"</html>"];
+                } else {
+                    [html appendFormat:@"<html><body bgcolor='#EEEEEE'><center><img style='vertical-align: middle;height:98%%;max-width:98%%' src='data:image/svg+xml;utf8,%@'></center></body></html>", [fileString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLFragmentAllowedCharacterSet]]];
+                }
+                // feed the HTML
+                CFDictionaryRef properties = (__bridge CFDictionaryRef)@{};
+                QLPreviewRequestSetDataRepresentation(preview,
+                                                      (__bridge CFDataRef)[html dataUsingEncoding:stringEncoding],
+                                                      kUTTypeHTML,
+                                                      properties
+                                                      );
+            }
+        }
+    }
+    return noErr;
+}
+
+void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview)
+{
+    // Implement only if supported
+}
--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # QLSVGSprite
 A QuickLook plugin able to preview SVG sprites and SVG images.
+
+About
+------------
+SVG sprites are a special kind of SVG file, mostly unsupported by SVG editors or viewers. They are made of different symbols, each a simple image with an id. This QL plugin allows previewing the contents of a sprite SVG file.
+All the sprites are displayed within a grid, headed by the symbol name.
+
+Installation
+------------
+* Make sure you have a **QuickLook** folder under **~/Library/QuickLook**. If unsure, just go to a terminal and run:
+
+```
+mkdir ~/Library/QuickLook  # Create the folder, or give an error if it exists
+```
+* Download the DMG, open it and drag the generator into the QuickLook shortcut.
+It should be available immediately.
+*
+If, for some reason, the previewer does not work, open a terminal and refresh QuickLook plugins with the *qlmanager* utility:
+
+```
+qlmanager -r
+```
+
+Creating the Installer
+----------------------
+* Create Icons for the installer
+
+```
+./build_icons.sh QLSVGSprite.png
+```
+
+* Create DMG installer
+
+```
+../create-dmg/create-dmg \
+--volname "QLSVGSprite Installer" \
+--volicon "installer/QLSVGSprite.icns" \
+--background "installer/background.png" \
+--window-pos 200 120 \
+--window-size 600 400 \
+--icon "QLSVGSprite.qlgenerator" 150 190 \
+--ql-drop-link 450 185 \
+"QLSVGSprite-Installer.dmg" \
+"to_install/"
+```
+
+References
+----------
+* [https://github.com/andreyvit/create-dmg](https://github.com/andreyvit/create-dmg)
+* [https://github.com/p2/quicklook-csv](https://github.com/p2/quicklook-csv)
+
+License
+-------
+
+This work is [Apache 2](./LICENSE.txt) licensed: [NOTICE.txt](./NOTICE.txt).
